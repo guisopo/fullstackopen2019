0da6f26720262a40c099fd8c3614aaa6114ec805
guisopo <solerguillermo@hotmail.com>
2019-07-19 22:37:04 +0200

    4.4: finish step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likes = []
+  const reducer = (acumulator, currentValue) => acumulator + currentValue
+  
+  blogs.map(blog => likes.push(blog.likes))
+  return likes.reduce(reducer)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }

42ee96025ff696d8135879c1fa60c55e28732918
guisopo <solerguillermo@hotmail.com>
2019-07-26 19:25:45 +0200

    Remove try-catch

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,24 +2,18 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  try {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-  }
-  catch(exception) {
-    next(exception)
-  }
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  try {
-    const blog = new  Blog(request.body)
-    const result = await blog.save()
-    response.status(201).json(result)
-  }
-  catch(exception) {
-    next(exception)
-  }
+  const body = request.body
+
+  !body.likes ? body.likes = 0 : ''
+
+  const blog = new  Blog(body)
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter

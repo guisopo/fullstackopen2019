eab1e3ab4750abdc63ceb48e2ed6bc4c6bf75f25
guisopo <solerguillermo@hotmail.com>
2019-07-27 16:42:03 +0200

    Add PUT router

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -30,9 +30,33 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  try {
+    const updatedblog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.status(200).json(updatedblog)  
+  }
+  catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+  catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter

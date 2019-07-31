245786d00a00e173dac16797a1043a0f4644ad79
guisopo <solerguillermo@hotmail.com>
2019-07-31 19:10:13 +0200

    4.17: expand blog router to designate user to the new blog created

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -8,6 +9,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
   
   if (!body.title || !body.url) {
     response.status(400).end()
@@ -18,11 +21,14 @@ blogsRouter.post('/', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   try {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   }
   catch(exception) {

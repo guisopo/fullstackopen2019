c9f1963c982059f47b1bd347faf802a51032e10a
guisopo <solerguillermo@hotmail.com>
2019-08-01 09:40:46 +0200

    4.18: Limit creating new notes to logged in users

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,21 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate( 'user', { userName: 1, name: 1   })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowercase().startsWidth('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-  
   if (!body.title || !body.url) {
     response.status(400).end()
     return
   }
+
+  const token = getTokenFrom(request)
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if(!token || !decodedToken.id) {
+      return response
+        .status(401)
+        .json({ error: 'token missing or invalid'})
+    }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -25,7 +45,6 @@ blogsRouter.post('/', async (request, response, next) => {
     user: user._id
   })
 
-  try {
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()

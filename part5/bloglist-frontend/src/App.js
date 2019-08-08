8ddabc5d08edcbabe7ddd921fa693bfbb5a3eaaa
guisopo <solerguillermo@hotmail.com>
2019-08-08 20:59:35 +0200

    5.1: create login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Log in to application</h1>
+      <form>
+        <div>
+          username:
+          <input 
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target)}
+          />
+        </div>
+        <div>
+          password:
+          <input 
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target)}
+          />
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;

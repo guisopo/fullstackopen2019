e1275bfc8bc13a853ada753105e60d8240e10fe2
guisopo <solerguillermo@hotmail.com>
2019-08-14 17:03:44 +0200

    5.7: add update blog route

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -12,8 +12,18 @@ const createBlog = async (newObject, token) => {
   }
 
   const response = await axios.post(baseUrl, newObject, config)
+
+  return response.data
+}
+
+const updateBlog = async (id, newObject, token) => {
+  const config = {
+    headers: { Authorization: `bearer ${token}` }
+  }
+
+  const response = await axios.put(`${baseUrl}/${id}`, newObject, config)
   console.log(response)
   return response.data
 }
 
-export default { getAll, createBlog }
+export default { getAll, createBlog, updateBlog }

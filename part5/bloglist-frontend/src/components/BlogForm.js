f1aae1a547a42d7543d7293a4a525085267fc54c
guisopo <solerguillermo@hotmail.com>
2019-08-14 15:22:24 +0200

    5.5 move title to component

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -10,36 +10,39 @@ const BlogForm = ({
   handleSubmit
 }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        title:
-        <input
-          type="text"
-          value={title}
-          name="Title"
-          onChange={handleTitleChange}
-        />
-      </div>
-      <div>
-        author:
-        <input
-          type="text"
-          value={author}
-          name="Author"
-          onChange={handleAuthorChange}
-        />
-      </div>
-      <div>
-        url:
-        <input
-          type="text"
-          value={url}
-          name="Url"
-          onChange={handleUrlChange}
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
+    <div>
+      <h1>Create new</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={handleAuthorChange}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={handleUrlChange}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>s
+    </div>
   )
 }
 
